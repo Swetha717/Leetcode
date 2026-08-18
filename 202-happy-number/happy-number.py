bbefda6cbd3e9d011fa55d3536471c10c9fa89fc
Swetha717 <swetha11230@gmail.com>
2026-08-18 15:22:01 +0530

    Time: 3 ms (32.92%) | Memory: 19.3 MB (62.14%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,13 +1,10 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         hash_set=set()
-        while True:
-            if n==1:
-                return True
-            if n in hash_set:
-                return False
+        while n!=1 and n not in hash_set:
             hash_set.add(n)
             s=0
             for v in str(n):
                 s+=int(v)**2
             n=s
+        return n==1
