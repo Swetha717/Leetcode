60997863c79ab6dc51c8b318702048d6664dbd23
Swetha717 <swetha11230@gmail.com>
2026-08-18 13:21:43 +0530

    Time: 11 ms (77.30%) | Memory: 19.3 MB (75.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,3 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s)==sorted(t)
+        hash_map_s={}
+        hash_map_t={}
+        for v in s:
+            hash_map_s[v]=hash_map_s.get(v,0)+1
+        for v in t:
+            hash_map_t[v]=hash_map_t.get(v,0)+1
+        return hash_map_s==hash_map_t
