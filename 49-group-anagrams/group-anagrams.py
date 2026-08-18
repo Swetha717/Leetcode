0d47b8da7252148d6f1b0cea0c11fe0163d1516e
Swetha717 <swetha11230@gmail.com>
2026-08-18 13:30:02 +0530

    Time: 11 ms (82.56%) | Memory: 21.7 MB (92.84%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hash_map={}
+        for v in strs:
+            key="".join(sorted(v))
+            if key in hash_map:
+                hash_map[key].append(v)
+            else:
+                hash_map[key]=[v]
+        return list(hash_map.values())
