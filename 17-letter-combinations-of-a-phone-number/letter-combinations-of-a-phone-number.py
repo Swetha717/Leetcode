c650e92cf7bf762fb926bfdb7e07c3bde6a15347
Swetha717 <swetha11230@gmail.com>
2026-08-20 14:32:39 +0530

    Time: 3 ms (3.68%) | Memory: 19.3 MB (40.72%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        hash_map={
+            '2':"abc",
+            '3':"def",
+            '4':"ghi",
+            '5':"jkl",
+            '6':"mno",
+            '7':"pqrs",
+            '8':"tuv",
+            '9':"wxyz"
+        }
+        res=[]
+        def backtrack(i,p):
+            if i==len(digits):
+                res.append(p)
+                return 
+            for c in hash_map[digits[i]]:
+                backtrack(i+1,p+c)
+        backtrack(0,"")
+        return res
+        
